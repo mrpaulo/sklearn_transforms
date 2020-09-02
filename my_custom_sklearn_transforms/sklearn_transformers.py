25953458d5cde763ee9fe47a11fa450f6f3772f0
Paulo Rodrigues <mr.paulo@aluno.faccat.br>
2020-09-02 20:38:58 -0300

    Criado Normalizer

--- my_custom_sklearn_transforms/sklearn_transformers.py
+++ my_custom_sklearn_transforms/sklearn_transformers.py
@@ -1,5 +1,5 @@
 from sklearn.base import BaseEstimator, TransformerMixin
-
+from sklearn.preprocessing import Normalizer
 
 # All sklearn Transforms must have the `transform` and `fit` methods
 class DropColumns(BaseEstimator, TransformerMixin):
@@ -12,5 +12,7 @@ class DropColumns(BaseEstimator, TransformerMixin):
     def transform(self, X):
         # Primeiro realizamos a cópia do dataframe 'X' de entrada
         data = X.copy()
+        data = Normalizer().fit_transform(data[["REPROVACOES_DE", "NOTA_DE"]])
         # Retornamos um novo dataframe sem as colunas indesejadas
         return data.drop(labels=self.columns, axis='columns')
+    
